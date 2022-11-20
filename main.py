99877dc6ebc722942e8d0042e99b92d05480e114
unknown <kelvemntcrtx@gmail.com>
2022-11-19 22:54:27 -0300

    corpo do game v1

---
+++ main.py
@@ -0,0 +1,43 @@
+###############################################################
+#                      Trabalho 02 Equipe                     #
+#             1- Kelve Monteiro Cartaxo - 542485              #
+#             2-Coloquem Seus Nomes                           #
+#             3-Coloquem Seus Nomes                           #
+#             4-Coloquem Seus Nomes                           #
+#             5-Coloquem Seus Nomes                           #
+###############################################################
+
+###########################
+#lugar dos imports
+import turtle
+from random import randint
+import os
+###########################
+
+
+
+
+
+
+
+
+
+
+
+
+###############################################################
+#Parte Do Kelve Programando o Sistema de batalha e testando algumas coisas aleatorias
+
+
+
+
+###############################################################
+
+window = turtle.Screen()
+
+
+
+
+
+
+window.mainloop()
